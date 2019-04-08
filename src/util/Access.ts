5e04215311be79b4e2449ac7c2d3cb0795ba2012
cca3 <cca3@st-andrews.ac.uk>
2019-04-08 14:34:55 +0100

    Add branch refs to fetch

--- src/util/Access.ts
+++ src/util/Access.ts
@@ -5,7 +5,7 @@ const apolloFetch = createApolloFetch({ uri: GITHUB_GRAPHQL_API });
 
 apolloFetch.use(({ request, options }, next) => {
   options.headers = {
-    authorization: "bearer 5e3c10cc6b8c68b3556f661dfe3d9d2bc1653a80"
+    authorization: "bearer "
   };
   next();
 });
@@ -15,7 +15,40 @@ export const fetchRepo = async (
   repoName: String,
   branchName: String
 ) => {
-  const query = `query {
+  const queryBranches = `query {
+    repository(owner: "${ownerName}", name: "${repoName}") {
+      refs(first: 10, , refPrefix:"refs/") {
+        nodes {
+          name
+        }
+      }
+      ref(qualifiedName: "${branchName}") {
+        target {
+          ... on Commit {
+            id
+            history(first: 99) {
+              pageInfo {
+                hasNextPage
+              }
+              edges {
+                node {
+                  messageHeadline
+                  oid
+                  message
+                  author {
+                    name
+                    email
+                    date
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }`;
+  const queryCommits = `query {
                       repository(owner: "${ownerName}", name: "${repoName}") {
                         ref(qualifiedName: "${branchName}") {
                           target {
@@ -45,7 +78,7 @@ export const fetchRepo = async (
                     }`;
   try {
     return apolloFetch({
-      query
+      query: queryBranches
     });
   } catch (error) {
     console.log(error);
